ed112fb5130637b72e72c350faf20a020a216989
timmson <timmson666@mail.ru>
2016-09-02 19:13:23 +0300

    Some features

--- index.js
+++ index.js
@@ -16,77 +16,88 @@ function LoanSchedule(options) {
 
     this.date = {
         format: 'DD.MM.YYYY'
-    }
-
-
-    this.fullAmount = new Decimal(options.amount);
-    this.interestRatePerYear = new Decimal(options.rate).div(100);
-    this.interestRatePeriod = 12; // 1/12
-    this.issueDate = options.issueDate;
-    this.term = new Decimal(options.term);
-
-
+    };
 }
-LoanSchedule.prototype.calculateAnnuitySchedule = function () {
+
+LoanSchedule.prototype.calculateAnnuitySchedule = function (options) {
     var paymentArray = [];
-    var paymentAmount = new Decimal(this.calculateAnnuityPaymentAmount());
-    var date = Moment();
-    for (var i = 0; i <= this.term.toNumber(); i++) {
-        var pay = {};
+    var date = Moment(options.issueDate, this.date.format);
+    var term = new Decimal(options.term);
+    var amount = new Decimal(options.amount);
+    var rate = new Decimal(options.rate);
+    var interestAccruedAmount = new Decimal(0);
+    var paymentAmount = new Decimal(this.calculateAnnuityPaymentAmount(options.amount, options.term, options.rate));
+    var i = 0;
+
+    while (i <= term.toNumber()) {
+
+        var pay = {
+            interestRate: rate.toFixed(this.money.decimal)
+        };
+
         if (i == 0) {
             pay.paymentDate = date.format(this.date.format);
             pay.initialBalance = new Decimal(0).toFixed(this.money.decimal);
             pay.paymentAmount = new Decimal(0).toFixed(this.money.decimal);
             pay.interestAmount = new Decimal(0).toFixed(this.money.decimal);
             pay.principalAmount = new Decimal(0).toFixed(this.money.decimal);
-            pay.finalBalance = this.fullAmount.toFixed(this.money.decimal);
-        } else if (i != this.term.toNumber()) {
-            date = date.add(1, 'months');
+            pay.finalBalance = new Decimal(amount).toFixed(this.money.decimal);
+        } else {
+            date = date.add(1, 'months').date(options.paymentOnDay);
             pay.paymentDate = date.format(this.date.format);
             pay.initialBalance = paymentArray[i - 1].finalBalance;
-            pay.interestAmount = this.calculateInterestByPeriod(paymentArray[i - 1].paymentDate, pay.paymentDate, pay.initialBalance);
-            pay.principalAmount = paymentAmount.minus(new Decimal(pay.interestAmount)).toFixed(this.money.decimal);
-            pay.paymentAmount = paymentAmount.toFixed(this.money.decimal);
-            pay.finalBalance = new Decimal(pay.initialBalance).minus(new Decimal(pay.principalAmount)).toFixed(this.money.decimal);
-        } else {
-            var lastDate = date.add(1, 'months');
-            pay.paymentDate = lastDate.format(this.date.format)
-            pay.initialBalance = paymentArray[i - 1].finalBalance;
-            pay.interestAmount = this.calculateInterestByPeriod(paymentArray[i - 1].paymentDate, pay.paymentDate, pay.initialBalance);
-            pay.principalAmount = pay.initialBalance;
-            pay.paymentAmount = new Decimal(pay.principalAmount).plus(new Decimal(pay.interestAmount)).toFixed(this.money.decimal);
+            interestAccruedAmount = interestAccruedAmount.plus(
+                this.calculateInterestByPeriod(paymentArray[i - 1].paymentDate, pay.paymentDate, pay.initialBalance, pay.interestRate)
+            );
+            if (i != term.toNumber()) {
+                if (interestAccruedAmount.gt(paymentAmount)) {
+                    pay.interestAmount = paymentAmount.toFixed(this.money.decimal);
+                    interestAccruedAmount = interestAccruedAmount.minus(paymentAmount);
+                } else {
+                    pay.interestAmount = interestAccruedAmount.toFixed(this.money.decimal);
+                    interestAccruedAmount = Decimal(0);
+                }
+                pay.principalAmount = paymentAmount.minus(new Decimal(pay.interestAmount)).toFixed(this.money.decimal);
+                pay.paymentAmount = paymentAmount.toFixed(this.money.decimal);
+            } else {
+                pay.interestAmount = interestAccruedAmount.toFixed(this.money.decimal);
+                pay.principalAmount = pay.initialBalance;
+                pay.paymentAmount = new Decimal(pay.principalAmount).plus(new Decimal(pay.interestAmount)).toFixed(this.money.decimal);
+            }
             pay.finalBalance = new Decimal(pay.initialBalance).minus(new Decimal(pay.principalAmount)).toFixed(this.money.decimal);
         }
+
         paymentArray.push(pay);
+        i++;
     }
     return paymentArray;
 
-};
+}
+;
 
-LoanSchedule.prototype.calculateAnnuityPaymentAmount = function () {
-    var termInPeriod = this.term;
-    var ratePerPeriod = this.interestRatePerYear.div(this.interestRatePeriod);
-    var paymentAmount = this.fullAmount.mul(ratePerPeriod.div(ratePerPeriod.plus(1).pow(termInPeriod.neg()).neg().plus(1)));
+LoanSchedule.prototype.calculateAnnuityPaymentAmount = function (amount, term, rate) {
+    var term = new Decimal(term);
+    var interestRate = new Decimal(rate).div(100).div(12);
+    var paymentAmount = new Decimal(amount).mul(interestRate.div(interestRate.plus(1).pow(term.neg()).neg().plus(1)));
     return paymentAmount.toFixed(this.money.decimal);
 };
 
-LoanSchedule.prototype.calculateInterestByPeriod = function (dateFrom, dateTo, balanceStr) {
+LoanSchedule.prototype.calculateInterestByPeriod = function (dateFrom, dateTo, balance, rate) {
     var curIntr = new Decimal(0);
     var dtFr = Moment(dateFrom, this.date.format);
     var dtTo = Moment(dateTo, this.date.format);
-    var balance = new Decimal(balanceStr);
     if (dtFr.isSame(dtTo, 'year')) {
-        curIntr = curIntr.plus(this.getInterestByPeriod(dtFr, dtTo, balance));
+        curIntr = curIntr.plus(this.getInterestByPeriod(dtFr, dtTo, balance, rate));
     } else {
         var endOfYear = Moment({years: dtFr.year(), months: 11, days: 31});
-        curIntr = curIntr.plus(new Decimal(this.getInterestByPeriod(dtFr, endOfYear, balance)));
-        curIntr = curIntr.plus(new Decimal(this.getInterestByPeriod(endOfYear, dtTo, balance)));
+        curIntr = curIntr.plus(new Decimal(this.getInterestByPeriod(dtFr, endOfYear, balance, rate)));
+        curIntr = curIntr.plus(new Decimal(this.getInterestByPeriod(endOfYear, dtTo, balance, rate)));
     }
     return curIntr.toFixed(this.money.decimal);
 };
 
-LoanSchedule.prototype.getInterestByPeriod = function (dateFrom, dateTo, balance) {
-    return this.interestRatePerYear.div(dateTo.year() % 4 == 0 ? 366 : 365).mul(Moment.duration(dateTo.diff(dateFrom)).asDays()).mul(balance);
+LoanSchedule.prototype.getInterestByPeriod = function (dateFrom, dateTo, balance, rate) {
+    return new Decimal(rate).div(100).div(dateTo.year() % 4 == 0 ? 366 : 365).mul(Moment.duration(dateTo.diff(dateFrom)).asDays()).mul(balance).toFixed(this.money.decimal);
 };
 
 //accounting.formatMoney(number, {symbol: 'руб.', format: '%v %s', thousand: ' '})
--- test.js
+++ test.js
@@ -1,23 +1,28 @@
 "use strict";
-const assert =  require('assert');
+const assert = require('assert');
 var LoanSchedule = require('./index.js');
-var Moment = require("moment");
+var Decimal = require("decimal.js");
 
-var loanSchedule = new LoanSchedule({
-    amount : 110000,
-    rate : 12,
-    term : 60
-});
+var loanSchedule = new LoanSchedule();
 
-/*assert.equal(loanSchedule.calculateAnnuityPaymentAmount(), '9773.37');
-assert.equal(loanSchedule.calculateInterestByPeriod('10.12.2015' , '10.01.2016', 1000), '10.18');
-assert.equal(loanSchedule.calculateInterestByPeriod('10.11.2015' , '10.12.2015', 1000), '9.86');*/
-loanSchedule.calculateAnnuitySchedule().forEach(function (pay) {
-    console.log(pay.paymentDate + '\t|\t\t'
-        + pay.initialBalance + '\t|\t\t'
-        + pay.paymentAmount + '\t|\t\t'
-        + pay.principalAmount + '\t|\t\t'
-        + pay.interestAmount + '\t|\t\t'
-        + pay.finalBalance
-    );
+assert.equal(loanSchedule.calculateAnnuityPaymentAmount(110000, 60, 12), '2446.89');
+assert.equal(loanSchedule.calculateInterestByPeriod('10.12.2015', '10.01.2016', 1000, 16.7), '14.17');
+assert.equal(loanSchedule.calculateInterestByPeriod('10.11.2015', '10.12.2015', 1000, 16.8), '13.81');
+var interestSum = new Decimal(0);
+loanSchedule.calculateAnnuitySchedule({
+    amount: 50000,
+    rate: 11.5,
+    term: 12,
+    paymentOnDay: 25,
+    issueDate: '25.10.2016'
+}).forEach(function (pay) {
+    interestSum = interestSum.plus(pay.interestAmount);
+    /*console.log(pay.paymentDate + '\t|\t\t'
+     + pay.initialBalance + '\t|\t\t'
+     + pay.paymentAmount + '\t|\t\t'
+     + pay.principalAmount + '\t|\t\t'
+     + pay.interestAmount + '\t|\t\t'
+     + pay.finalBalance
+     );*/
 });
+assert.equal(interestSum.toFixed(2), '3165.39');
