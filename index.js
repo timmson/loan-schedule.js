abfde851d7dfd3ae45e24edcf08a01b294499dbf
Tim Biden <timbiden@gmail.com>
2019-11-20 12:44:58 -0800

    Done with the notes.
    I think.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Call 3 loan types.
+// They all call Abstract Loan Schedule
 const AnnuitySchedule = require("./lib/annuity-loan-schedule");
 const BubbleLoanSchedule = require("./lib/bubble-loan-schedule");
 const DifferentiatedSchedule  = require("./lib/differentiated-loan-schedule");
@@ -9,6 +11,7 @@ const mapping = {};
 class LoanSchedule {
 	
 	// What does a `static` method do and why?
+	// getLoanSchedule is only used here.
 	static getLoanSchedule(scheduleType, options) {
 		if (Object.prototype.hasOwnProperty.call(mapping, scheduleType)) {
 			return new mapping[scheduleType](options);
@@ -29,16 +32,19 @@ class LoanSchedule {
 	}
 
 	// Calculate Interest By Period
+	// Another function of same name in Abstract Loan Schedule
 	calculateInterestByPeriod(p) {
 		return new AnnuitySchedule(this.options).calculateInterestByPeriod(p);
 	}
 
 	// Calculate payment amount
+	// Another function of same name in /annuity-loan-schedule
 	calculateAnnuityPaymentAmount(p) {
 		return new AnnuitySchedule(this.options).calculateAnnuityPaymentAmount(p);
 	}
 
 	// Calculate Max Loan Amount
+	// Another function of same name in /annuity-loan-schedule
 	calculateMaxLoanAmount(p) {
 		return new AnnuitySchedule(this.options).calculateMaxLoanAmount(p);
 	}
@@ -50,6 +56,7 @@ LoanSchedule.DIFFERENTIATED_SCHEDULE = "DIFFERENTIATED";
 LoanSchedule.BUBBLE_SCHEDULE = "BUBBLE";
 
 // Essentially copying imports to an object? Huh?
+// What does `mapping` do?
 mapping[LoanSchedule.ANNUITY_SCHEDULE] = AnnuitySchedule;
 mapping[LoanSchedule.BUBBLE_SCHEDULE] = BubbleLoanSchedule;
 mapping[LoanSchedule.DIFFERENTIATED_SCHEDULE] = DifferentiatedSchedule;
