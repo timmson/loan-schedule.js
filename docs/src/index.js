ea873c5b165df5581f0bb8d25d6940a970c85d72
timmson <timmson666@mail.ru>
2019-12-17 00:46:33 +0300

    Add removing of early repayment #21

--- docs/src/index.js
+++ docs/src/index.js
@@ -39,10 +39,8 @@ let app = new Vue({
             this.request.amount = this.schedule.payments[paymentId - 1].finalBalance;
             this.request.term = this.request.term - paymentId - 1;
             this.request.issueDate = this.schedule.payments[paymentId - 1].paymentDate;
-            //this.request.paymentAmount = this.schedule.payments[paymentId].paymentAmount;
             this.request.earlyRepayment = {};
             this.updateSchedule(null);
-
         },
         addEarlyRepayment: function (event) {
             this.request.earlyRepayment[this.earlyRepayment.date] = {
