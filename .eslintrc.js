49231df7c9a39a9c22fd4e8f5dd7a9bd654066ce
timmson <timmson666@mail.ru>
2020-04-27 23:49:44 +0300

    Fix design

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "mocha": true
-    },
-    "extends": [
-        "eslint:recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es6": true,
+		"mocha": true
+	},
+	"extends": [
+		"eslint:recommended"
+	],
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly"
+	},
+	"parserOptions": {
+		"ecmaVersion": 2018
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
 };
--- docs/src/index.js
+++ docs/src/index.js
@@ -9,68 +9,67 @@ import Vue from "vue";
 
 
 let loanSchedule = new LoanSchedule({
-    prodCalendar: "ru"
+	prodCalendar: "ru"
 });
 
 let url = URL.parse(window.location.href);
 let params = QueryString.parse(url.query);
 
 let request = {
-    amount: params["amount"] || 2000000,
-    rate: params["rate"] || 9.5,
-    term: params["term"] || 240,
-    paymentAmount: params["paymentAmount"],
-    issueDate: params["issueDate"] || new Intl.DateTimeFormat("ru").format(new Date()),
-    paymentOnDay: params["paymentOnDay"] || 1,
-    scheduleType: LoanSchedule.ANNUITY_SCHEDULE,
-    earlyRepayment: {}
+	amount: params["amount"] || 2000000,
+	rate: params["rate"] || 9.5,
+	term: params["term"] || 240,
+	paymentAmount: params["paymentAmount"],
+	issueDate: params["issueDate"] || new Intl.DateTimeFormat("ru").format(new Date()),
+	paymentOnDay: params["paymentOnDay"] || 1,
+	scheduleType: LoanSchedule.ANNUITY_SCHEDULE,
+	earlyRepayment: {}
 };
 
 let app = new Vue({
-    el: '#app',
-    data: {
-        currentYear: new Date().getFullYear().toString(),
-        request: request,
-        earlyRepayment: {
-            date: "",
-            amount: ""
-        },
-        schedule: {}
-    },
-    methods: {
-        updateSchedule: function (event) {
-            this.schedule = loanSchedule.calculateSchedule(this.request);
-            this.schedule.lastPaymentDate = this.schedule.payments[this.schedule.payments.length - 1].paymentDate;
-            this.schedule.termInYear = Math.ceil(this.schedule.term / 12);
-            window.history.replaceState({}, "Loan Amortization Schedule", "?" + QueryString.stringify(this.request));
-        },
-        copyPayment: function (event, paymentId) {
-            this.request.amount = this.schedule.payments[paymentId - 1].finalBalance;
-            this.request.term = this.request.term - paymentId + 1;
-            this.request.issueDate = this.schedule.payments[paymentId - 1].paymentDate;
-            this.request.earlyRepayment = {};
-            this.updateSchedule(null);
-        },
-        addEarlyRepayment: function (event) {
-            this.request.earlyRepayment[this.earlyRepayment.date] = {
-                "erAmount": this.earlyRepayment.amount,
-                "erType": LoanSchedule.ER_TYPE_MATURITY
-            };
-            this.updateSchedule(null);
-        },
-        resetEarlyPayment: function (event, date) {
-            delete this.request.earlyRepayment[date];
-            this.updateSchedule(null);
-        },
-        toMoney: function (number) {
-            return Accounting.formatMoney(number, {symbol: "", format: "%s%v", thousand: " "});
-        }
-    },
-    mounted() {
-        this.updateSchedule(null);
-    },
-    beforeUpdate() {
-        this.request.paymentOnDay = parseInt(this.request.paymentOnDay) > 28 ? 28 : this.request.paymentOnDay;
-    }
+	el: '#app',
+	data: {
+		currentYear: new Date().getFullYear().toString(),
+		request: request,
+		earlyRepayment: {
+			date: "",
+			amount: ""
+		},
+		schedule: {}
+	},
+	methods: {
+		updateSchedule: function (event) {
+			if (this.earlyRepayment.date) {
+				delete this.request.earlyRepayment[this.earlyRepayment.date];
+				if (this.earlyRepayment.amount) {
+					this.request.earlyRepayment[this.earlyRepayment.date] = {
+						"erAmount": this.earlyRepayment.amount,
+						"erType": LoanSchedule.ER_TYPE_MATURITY
+					};
+				}
+			}
+
+			this.schedule = loanSchedule.calculateSchedule(this.request);
+			this.schedule.lastPaymentDate = this.schedule.payments[this.schedule.payments.length - 1].paymentDate;
+			this.schedule.termInYear = Math.ceil(this.schedule.term / 12);
+			window.history.replaceState({}, "Loan Amortization Schedule", "?" + QueryString.stringify(this.request));
+		},
+		copyPayment: function (event, paymentId) {
+			this.request.amount = this.schedule.payments[paymentId - 1].finalBalance;
+			this.request.term = this.request.term - paymentId + 1;
+			this.request.issueDate = this.schedule.payments[paymentId - 1].paymentDate;
+			this.request.earlyRepayment = {};
+			this.updateSchedule(null);
+		},
+		toMoney: function (number) {
+			return Accounting.formatMoney(number, {symbol: "", format: "%s%v", thousand: " "});
+		}
+	},
+	mounted() {
+		this.updateSchedule(null);
+	},
+	beforeUpdate() {
+		this.request.paymentOnDay = parseInt(this.request.paymentOnDay) > 28 ? 28 : this.request.paymentOnDay;
+	}
 });
 
