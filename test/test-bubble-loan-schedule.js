12636a7d9e9fdacb0dd894e75806afb7b3008d10
timmson <timmson666@mail.ru>
2020-04-28 22:22:59 +0300

    Fix diff payments

--- test/test-bubble-loan-schedule.js
+++ test/test-bubble-loan-schedule.js
@@ -6,12 +6,14 @@ describe("Bubble Loan Schedule should", () => {
 	let loanSchedule = new LoanSchedule();
 
 	it("have overall interest eq 5747.13 when params($50000/12m/11.5%/25.10.2016/25)", () => {
-		expect(loanSchedule.calculateSchedule({
+		let schedule = loanSchedule.calculateSchedule({
 			amount: 50000,
 			rate: 11.5,
 			term: 12,
 			paymentOnDay: 25,
 			issueDate: "25.10.2016"
-		}).overAllInterest, "5747.13");
+		});
+
+		expect(schedule.overAllInterest).to.equal("5747.13");
 	});
 });
--- test/test-diffirentiated-loan-schedule.js
+++ test/test-diffirentiated-loan-schedule.js
@@ -6,12 +6,15 @@ describe("Differentiated Loan Schedule should", () => {
 	let loanSchedule = new LoanSchedule();
 
 	it("have overall interest eq 3111.18 when params($50000/12m/11.5%/25.10.2016/25)", () => {
-		expect(loanSchedule.calculateSchedule({
+		let schedule = loanSchedule.calculateSchedule({
 			amount: 50000,
 			rate: 11.5,
 			term: 12,
 			paymentOnDay: 25,
 			issueDate: "25.10.2016"
-		}).overAllInterest, "3111.18");
+		});
+
+		expect(schedule.payments[schedule.payments.length - 1].paymentAmount).to.equal("4206.01");
+		expect(schedule.overAllInterest).to.equal("3111.18");
 	});
 });
