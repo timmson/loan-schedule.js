2981b61e73d1e7cac680015cea4693535922009e
timmson <timmson666@mail.ru>
2021-03-02 23:39:08 +0300

    Add auto format number

--- docs/src/index.js
+++ docs/src/index.js
@@ -27,7 +27,7 @@ let request = {
 };
 
 let app = new Vue({
-	el: '#app',
+	el: "#app",
 	data: {
 		currentYear: new Date().getFullYear().toString(),
 		request: request,
@@ -38,12 +38,18 @@ let app = new Vue({
 		schedule: {}
 	},
 	methods: {
+
 		updateSchedule: function (event) {
+
+			this.request.amount = this.fromMoney(this.request.amount);
+			this.request.paymentAmount = this.fromMoney(this.request.paymentAmount);
+			this.request.paymentOnDay = parseInt(this.request.paymentOnDay) > 28 ? 28 : this.request.paymentOnDay;
+
 			if (this.earlyRepayment.date) {
 				delete this.request.earlyRepayment[this.earlyRepayment.date];
 				if (this.earlyRepayment.amount) {
 					this.request.earlyRepayment[this.earlyRepayment.date] = {
-						"erAmount": this.earlyRepayment.amount,
+						"erAmount": this.fromMoney(this.earlyRepayment.amount),
 						"erType": LoanSchedule.ER_TYPE_MATURITY
 					};
 				}
@@ -53,7 +59,12 @@ let app = new Vue({
 			this.schedule.lastPaymentDate = this.schedule.payments[this.schedule.payments.length - 1].paymentDate;
 			this.schedule.termInYear = Math.ceil(this.schedule.term / 12);
 			window.history.replaceState({}, "Loan Amortization Schedule", "?" + QueryString.stringify(this.request));
+
+			this.request.amount = this.toMoney(this.request.amount);
+			this.request.paymentAmount = this.toMoney(this.request.paymentAmount);
+			this.earlyRepayment.amount = this.toMoney(this.earlyRepayment.amount);
 		},
+
 		copyPayment: function (event, paymentId) {
 			this.request.amount = this.schedule.payments[paymentId - 1].finalBalance;
 			this.request.term = this.request.term - paymentId + 1;
@@ -61,15 +72,18 @@ let app = new Vue({
 			this.request.earlyRepayment = {};
 			this.updateSchedule(null);
 		},
+
 		toMoney: function (number) {
-			return Accounting.formatMoney(number, {symbol: "", format: "%s%v", thousand: " "});
+			return number ? Accounting.formatMoney(number, {symbol: "", format: "%s%v", thousand: " "}) : number;
+		},
+
+		fromMoney: function (numberWithSpaces) {
+			return numberWithSpaces ? numberWithSpaces.toString().split(" ").join("") : numberWithSpaces;
 		}
 	},
+
 	mounted() {
 		this.updateSchedule(null);
-	},
-	beforeUpdate() {
-		this.request.paymentOnDay = parseInt(this.request.paymentOnDay) > 28 ? 28 : this.request.paymentOnDay;
 	}
 });
 
