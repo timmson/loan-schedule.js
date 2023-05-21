24522976c18703b74bf12ffbee1afe8690e35c57
timmson <timmson666@mail.ru>
2023-05-21 10:01:28 +0300

    Update dependencies

--- babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	plugins: ["babel-plugin-replace-ts-export-assignment"],
-	presets: ["@babel/preset-env", "@babel/preset-typescript"],
-};
--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	collectCoverage: true,
-	collectCoverageFrom: ["./dist/*"],
-	coverageReporters: ["lcov"],
-	testMatch: ["**/test/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"]
-}
